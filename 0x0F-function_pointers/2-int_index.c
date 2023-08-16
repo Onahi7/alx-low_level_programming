eb59620729ce3a5cd6c420e23e5a102803886398
Onahi7 <dicksonhardy7@gmail.com>
2023-08-16 16:33:02 -0700

    final

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,25 +1,22 @@
 #include "function_pointers.h"
-
 /**
- * int_index - searches for an integer in @array
- * @array: pointer to the array of numbers.
- * @size: size of array.
- * @cmp: pointer to comparation function
- * Return: the index of array.
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-        int i;
+	int i;
 
-        if (size <= 0)
-                return (-1);
-        if (!array || !cmp)
-                return (-1);
-        for (i = 0; i < size; i++)
-        {
-                if (cmp(array[i]) != 0)
-                        return (i);
-        }
-        return (-1);
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,20 +1,14 @@
-#ifndef _CALC_H_
-#define _CALC_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-
+#ifndef CALC_H
+#define CALC_H
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
-
 typedef struct op
 {
-        char *op;
-        int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
