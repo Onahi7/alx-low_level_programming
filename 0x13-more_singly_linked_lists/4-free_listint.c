cdde4e0d0dd39eb495499c2a49ca47289dfb70a0
Onahi7 <dicksonhardy7@gmail.com>
2023-08-29 15:27:55 -0700

    final commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+i#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: a pointer to the head of the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
